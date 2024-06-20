9a7c08c4c11d518388edfbace7ab71af8f8b1f57
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-20 09:38:06 +0000

    made a summation webpage

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function findIntegerClicked() {
+  const integer = parseInt(document.getElementById("num-entered").value)
+  let answer = 0
+  for (let counter = 1; counter <= integer; counter++) {
+    answer += Math.abs(counter)
+    console.log(counter)
+  }
+  document.getElementById("answer").innerHTML = "Your answer is: " + answer
+}
